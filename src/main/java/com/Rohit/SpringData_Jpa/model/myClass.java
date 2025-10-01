c77e97d7828cdeb5a071bc8c603390ac3fcbb5cd
Rohit <rohitdrk22@gmail.com>
2025-10-01 16:50:53 +0530

    myClass is being added from intellij ide

---
+++ src/main/java/com/Rohit/SpringData_Jpa/model/myClass.java
@@ -0,0 +1,11 @@
+package com.Rohit.SpringData_Jpa.model;
+
+public class myClass {
+
+    public myclass()
+    {
+        System.out.println("this is the new class from intellij ide");
+    }
+
+
+}
